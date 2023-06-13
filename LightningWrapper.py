e08d5e93e44c226a131f7ac981149974e2b7d162
Karol Wojtulewicz <karol.wojtulewicz@liu.se>
2023-06-13 07:04:04 -0400

    Added even more option

--- LightningWrapper.py
+++ LightningWrapper.py
@@ -124,18 +124,37 @@ if __name__ == "__main__":
     from pytorch_lightning import Trainer
     from pytorch_lightning.loggers import TensorBoardLogger
     from torch.utils.data import DataLoader
+    from LightningWrapper import LightningWrapper
+    from torchvision.models import resnet50
+    from torch.nn import CrossEntropyLoss
+    from torch.optim import Adam
+    from pytorch_lightning import Trainer
+    from pytorch_lightning.loggers import TensorBoardLogger
+    from torch.utils.data import DataLoader, random_split
+    from torchvision.datasets import CIFAR10
+    import torchvision.transforms as transforms
 
-    # Load your data here
+    # Your data here. For this example, I will use CIFAR10
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    # Initialize your model, loss function and optional metric function here
-    opt = ModelOptions()
-    model = None # Your model here
-    loss_func = None # Your loss function here
-    metric_fc = None # Your metric function here
-    your_data = None # Your data here
+    trainset = CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = DataLoader(trainset, batch_size=4,
+                                            shuffle=True, num_workers=2)
 
-    wrapper = LightningWrapper(opt, model, loss_func, metric_fc)
-    logger = TensorBoardLogger('tb_logs', name='my_model')
-    trainer = Trainer(logger=logger)
-    trainer.fit(wrapper, DataLoader(your_data))
+    # Model and Loss function
+    model = resnet50(pretrained=True)  # Pretrained ResNet50 model
+    loss_func = CrossEntropyLoss()  # Cross-entropy loss function
+
+    opt = ModelOptions()  # Your model options
+    wrapper = LightningWrapper(opt, model, loss_func)  # Wrap your model with the PyTorch Lightning Wrapper
+
+    # Define the logger for TensorBoard
+    logger = TensorBoardLogger(save_dir=opt.logging_dir, name=opt.logging_name)
+
+    # Initialize the PyTorch Lightning trainer and start training
+    trainer = Trainer(max_epochs=3, logger=logger)
+    trainer.fit(wrapper, trainloader)
 
--- ModelOptions.py
+++ ModelOptions.py
@@ -12,6 +12,21 @@ class ModelOptions:
             self.weight_decay = 0.0005  # weight decay for optimizer
             self.lr_step = 10  # step size for learning rate scheduler
             self.optimizer = 'adam'  # type of optimizer to use
+            self.model_architecture = 'resnet50'  # model architecture
+            self.pretrained = True  # use pre-trained model or not
+            self.num_epochs = 100  # number of training epochs
+            self.batch_size = 64  # batch size for training
+            self.num_workers = 4  # number of workers for data loading
+            self.shuffle = True  # shuffle dataset for each epoch or not
+            self.pin_memory = True  # pin memory for data loading or not
+            self.dropout = 0.5  # dropout rate
+            self.activation = 'relu'  # activation function to use
+            self.loss_function = 'CrossEntropyLoss'  # loss function to use
+            self.metrics = ['accuracy']  # metrics to track
+            self.logging_dir = 'tb_logs'  # directory for TensorBoard logs
+            self.logging_name = 'my_model'  # name of your model for logging purposes
+            self.save_model_path = './model.pth'  # path to save the model
+
 
 if __name__ == "__main__":
     opt = ModelOptions(path='model_options.yml')
