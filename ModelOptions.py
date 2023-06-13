8cc82f53450ee5344f78ef57a05494042a9c440b
Karol Wojtulewicz <karol.wojtulewicz@liu.se>
2023-06-13 06:55:26 -0400

    Added more options for model options

--- ModelOptions.py
+++ ModelOptions.py
@@ -1,20 +1,18 @@
+import yaml
+
 class ModelOptions:
-    def __init__(self):
-        # Model options
-        self.backbone = 'resnet50'  # model backbone
-        self.num_classes = 1000  # number of classes in your dataset
+    def __init__(self, path=None):
+        if path is not None:
+            with open(path, 'r') as file:
+                options = yaml.safe_load(file)
+            self.__dict__.update(options)
+        else:
+            # default settings
+            self.lr = 0.01  # learning rate
+            self.weight_decay = 0.0005  # weight decay for optimizer
+            self.lr_step = 10  # step size for learning rate scheduler
+            self.optimizer = 'adam'  # type of optimizer to use
 
-        # Loss options
-        self.loss = 'CrossEntropyLoss'  # type of loss function to use
-
-        # Training options
-        self.lr = 0.01  # learning rate
-        self.weight_decay = 0.0005  # weight decay for optimizer
-        self.lr_step = 10  # step size for learning rate scheduler
-
-        # Optimizer options
-        self.optimizer = 'sgd'  # type of optimizer to use
-
-        # Logging options
-        self.logging_dir = 'tb_logs'  # directory for TensorBoard logs
-        self.logging_name = 'my_model'  # name of your model for logging purposes
+if __name__ == "__main__":
+    opt = ModelOptions(path='model_options.yml')
+    print(opt.lr)
